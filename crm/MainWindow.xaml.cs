39efcf716adacba13fe134aac32113a7111d2225
Pasha_Vezdel <135170989+PaxaVzd@users.noreply.github.com>
2025-01-27 20:54:37 +0200

    delete button logic

--- crm/MainWindow.xaml.cs
+++ crm/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI;
 using System;
 using System.Data;
+using System.Text.RegularExpressions;
 using System.Windows;
 
 namespace GymCRM
@@ -52,6 +54,7 @@ namespace GymCRM
                 SubscriptionEndTextBox.Text = SelectedClient["subscription_end_date"].ToString();
                 CreationDateTextBox.Text = SelectedClient["created_at"].ToString();
                 BalanceTextBox.Text = SelectedClient["balance"].ToString();
+                CommentTextBox.Text = SelectedClient["comments"].ToString();
             }
         }
 
@@ -61,5 +64,77 @@ namespace GymCRM
             authWindow.Show();
             this.Close();
         }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (SelectedClient == null)
+            {
+                MessageBox.Show("Будь ласка, виберіть клієнта для видалення.");
+                return;
+            }
+
+            var result = MessageBox.Show("Ви впевнені, що хочете видалити цього клієнта?", "Підтвердження", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.No)
+            {
+                return;
+            }
+
+            try
+            {
+                string connectionString = DatabaseConfig.ConnectionString;
+                string query = "DELETE FROM Clients WHERE id = @id";
+
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    MySqlCommand command = new MySqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@id", SelectedClient["id"]);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+
+                    MessageBox.Show("Клієнт успішно видалений.");
+
+                    UpdateClientsList();
+                }
+
+                FullNameTextBox.Clear();
+                DateOfBirthTextBox.Clear();
+                PhoneNumberTextBox.Clear();
+                LastPaymentTextBox.Clear();
+                SubscriptionEndTextBox.Clear();
+                BalanceTextBox.Clear();
+                CommentTextBox.Clear();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка при видаленні клієнта: " + ex.Message);
+            }
+        }
+
+        private void UpdateClientsList()
+        {
+            string connectionString = DatabaseConfig.ConnectionString;
+            string query = "SELECT * FROM Clients";
+
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    MySqlDataAdapter dataAdapter = new MySqlDataAdapter(query, connection);
+                    DataTable dataTable = new DataTable();
+                    dataAdapter.Fill(dataTable);
+
+                    ClientsDataGrid.ItemsSource = dataTable.DefaultView;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка при оновленні списку клієнтів: " + ex.Message);
+            }
+        }
+
+
+
     }
 }
--- crm/obj/Debug/net8.0-windows/crm.AssemblyInfo.cs
+++ crm/obj/Debug/net8.0-windows/crm.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("crm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29043b6f454309b1832a3481b7337c445f863976")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5005711c5d335ebcf580391ee66afba6a72aa014")]
 [assembly: System.Reflection.AssemblyProductAttribute("crm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("crm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
