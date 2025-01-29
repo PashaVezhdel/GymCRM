31a813a7b2adfd4bd4a0d26cb1b67e371964b6bd
Pasha_Vezdel <135170989+PaxaVzd@users.noreply.github.com>
2025-01-29 17:10:51 +0200

    fix placeholder text

--- crm/obj/Debug/net8.0-windows/crm.AssemblyInfo.cs
+++ crm/obj/Debug/net8.0-windows/crm.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("crm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae378be02ce3699521083bbe5a065edc06740035")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80e9f7711471612d74214455354b96b2fb87b395")]
 [assembly: System.Reflection.AssemblyProductAttribute("crm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("crm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
