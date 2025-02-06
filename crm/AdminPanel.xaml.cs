6a5c0f004ca330f6059f521585444fd81ceb4361
Pasha_Vezdel <135170989+PaxaVzd@users.noreply.github.com>
2025-02-06 15:52:59 +0200

    Data parsing into text fields

--- crm/AdminPanel.xaml.cs
+++ crm/AdminPanel.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System;
-using System.Data;
-using System.Windows;
-using GymCRM;
+﻿using GymCRM;
 using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace crm
 {
@@ -16,23 +18,41 @@ namespace crm
 
         private void LoadUsersData()
         {
-            using (MySqlConnection connection = new MySqlConnection(DatabaseConfig.ConnectionString))
+            DatabaseConnection dbConnection = new DatabaseConnection();
+            if (dbConnection.Connect())  
             {
-                try
+                string query = "SELECT * FROM users";
+                using (MySqlConnection conn = new MySqlConnection(DatabaseConfig.ConnectionString))
                 {
-                    connection.Open();
-                    string query = "SELECT * FROM users";
-                    MySqlDataAdapter dataAdapter = new MySqlDataAdapter(query, connection);
+                    conn.Open();
+                    MySqlDataAdapter dataAdapter = new MySqlDataAdapter(query, conn);
                     DataTable dataTable = new DataTable();
                     dataAdapter.Fill(dataTable);
                     UsersDataGrid.ItemsSource = dataTable.DefaultView;
                 }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Помилка при завантаженні даних: " + ex.Message, "Помилка", MessageBoxButton.OK, MessageBoxImage.Error);
-                }
+            }
+            else
+            {
+                MessageBox.Show("Не вдалося підключитися до бази даних.");
+            }
+        }
+
+        private void UsersDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (UsersDataGrid.SelectedItem is DataRowView selectedRow)
+            {
+                UsernameTextBox.Text = selectedRow["username"].ToString();
+                PasswordBox.Password = selectedRow["password"].ToString(); 
+                RoleComboBox.SelectedItem = RoleComboBox.Items
+                    .Cast<ComboBoxItem>()
+                    .FirstOrDefault(item => item.Content.ToString() == selectedRow["role"].ToString());
+            }
+            else
+            {
+                UsernameTextBox.Clear();
+                PasswordBox.Clear();
+                RoleComboBox.SelectedIndex = -1;
             }
         }
     }
 }
-
